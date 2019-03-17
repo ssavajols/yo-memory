e845452a926addf11c4dbffba1b03a207ba10060
Sylvain Savajols <s.savajols@gmail.com>
2019-03-17 12:16:50 +0100

    Add log version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { AppComponent } from './app.component'
+import { CoreModule } from './core/core.module'
+import { SharedModule } from './shared/shared.module'
+import { ServiceWorkerModule } from '@angular/service-worker'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, SharedModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    CoreModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+console.log('Version: 1.0.0')
